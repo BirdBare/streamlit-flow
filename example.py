dae382f81622d9c8b116130e2b9ba12c7bd4a3a6
JJ-BradleyBare <bradleybare@gmail.com>
2026-03-06 19:47:48 -0500

    example with markers

--- example.py
+++ example.py
@@ -1,48 +1,93 @@
-import streamlit
+import streamlit as st
 
 import streamlit_flow
 
+handle_right_source = streamlit_flow.Handle("right", is_target=False)
+handle_left_target = streamlit_flow.Handle("left", is_source=False)
+handle_right_source.add_valid_targets(handle_left_target.id)
 
-def add_node():
-    new_node = streamlit_flow.MarkdownNode(
-        0,
-        50,
-        "New",
-        handle_ids={handle.id for handle in streamlit.session_state["handles"]},
-    )
+handles = [handle_right_source, handle_left_target]
 
-    streamlit.session_state["state"].nodes.append(new_node)
-
-
-if "state" not in streamlit.session_state:
-    state = streamlit.session_state["state"] = streamlit_flow.Diagram([], [], [], [])
-
-    handle_1 = streamlit_flow.Handle("top", style={"backgroundColor": "red"})
-    state.handles.append(handle_1)
-    handle_2 = streamlit_flow.Handle("top", style={"backgroundColor": "green"})
-    state.handles.append(handle_2)
-    handle_3 = streamlit_flow.Handle("bottom")
-    state.handles.append(handle_3)
-    handle_4 = streamlit_flow.Handle("left")
-    state.handles.append(handle_4)
-
-    handle_1.add_valid_targets(handle_2.id)
-
-    handles = streamlit.session_state["handles"] = [handle_1, handle_2, handle_3, handle_4]
-
-    node_1 = streamlit_flow.MarkdownNode(0, 50, "Hello", handle_ids={handle.id for handle in handles})
-    state.nodes.append(node_1)
-    node_2 = streamlit_flow.MarkdownNode(50, 0, "Pop", handle_ids={handle.id for handle in handles})
-    state.nodes.append(node_2)
-
-streamlit.button("Add", on_click=add_node)
-
-state = streamlit.session_state["state"] = streamlit_flow.render(
-    "A",
-    streamlit.session_state["state"],
-    allow_new_edges=True,
-    get_edge_on_click=True,
-    get_node_on_click=True,
+node_1 = streamlit_flow.MarkdownNode(
+    pos_x=100,
+    pos_y=100,
+    content="Node 1",
+    draggable=False,
+    handle_ids={handle_right_source.id},
 )
 
-print(state.selected_id)
+node_2 = streamlit_flow.MarkdownNode(
+    pos_x=350,
+    pos_y=50,
+    content="Node 2",
+    draggable=False,
+    handle_ids={handle_right_source.id, handle_left_target.id},
+)
+node_3 = streamlit_flow.MarkdownNode(
+    pos_x=350,
+    pos_y=150,
+    content="Node 3",
+    draggable=False,
+    handle_ids={handle_right_source.id, handle_left_target.id},
+)
+node_4 = streamlit_flow.MarkdownNode(
+    pos_x=600,
+    pos_y=100,
+    content="Node 4",
+    draggable=False,
+    handle_ids={handle_left_target.id},
+)
+nodes: list[streamlit_flow.BaseNode] = [node_1, node_2, node_3, node_4]
+
+marker_arrow = streamlit_flow.Marker("arrow")
+print(marker_arrow.id)
+markers = [marker_arrow]
+
+
+edges = [
+    streamlit_flow.Edge(
+        node_1.id,
+        handle_right_source.id,
+        node_2.id,
+        handle_left_target.id,
+        animated=True,
+        target_marker_id=marker_arrow.id,
+    ),
+    streamlit_flow.Edge(
+        node_1.id,
+        handle_right_source.id,
+        node_3.id,
+        handle_left_target.id,
+        animated=True,
+        target_marker_id=marker_arrow.id,
+    ),
+    streamlit_flow.Edge(
+        node_2.id,
+        handle_right_source.id,
+        node_4.id,
+        handle_left_target.id,
+        animated=True,
+        target_marker_id=marker_arrow.id,
+    ),
+    streamlit_flow.Edge(
+        node_3.id,
+        handle_right_source.id,
+        node_4.id,
+        handle_left_target.id,
+        animated=True,
+        target_marker_id=marker_arrow.id,
+    ),
+]
+
+if "static_flow_state" not in st.session_state:
+    st.session_state.static_flow_state = streamlit_flow.Diagram(nodes, handles, edges, markers)
+
+streamlit_flow.render(
+    "static_flow",
+    st.session_state.static_flow_state,
+    fit_view=True,
+    show_minimap=False,
+    show_controls=False,
+    pan_on_drag=False,
+    allow_zoom=False,
+)
